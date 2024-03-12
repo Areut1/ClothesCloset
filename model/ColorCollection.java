ebcfe6f1d04463a446166f245a2154ecb2dea506
matthewrothwell <mroth5@brockport.edu>
2024-03-12 12:57:18 -0400

    ColorCollection updated, EntityBase and ui classes added

--- model/ColorCollection.java
+++ model/ColorCollection.java
@@ -1,4 +1,100 @@
 package model;
 
-public class ColorCollection {
+import impresario.IView;
+
+import java.util.Properties;
+import java.util.Vector;
+
+public class ColorCollection extends EntityBase implements IView {
+    private static final String myTableName = "Book";
+    Vector<Color> colorList;
+
+    public ColorCollection() {
+        super(myTableName);
+        colorList = new Vector<>();
+    }
+
+//    public void findColors(Properties props) throws Exception {
+//        String query = "SELECT * FROM " + myTableName + " WHERE ";
+//
+//        //start generic query, add on for each requirement
+//        if (props.getProperty("bookTitle") != null){
+//            //add on to query
+//            query += "(bookTitle LIKE \"%" + props.getProperty("bookTitle") + "%\")";
+//        }
+//        if (props.getProperty("author") != null){
+//            //add on to query
+//            if (props.getProperty("bookTitle") != null){
+//                query += " AND ";
+//            }
+//            query += "(author = \"" + props.getProperty("author") + "\")";
+//
+//        }
+//        if (props.getProperty("pubYear") != null){
+//            //add on to query
+//            if ((props.getProperty("bookTitle") != null) || (props.getProperty("author") != null)){
+//                query += " AND ";
+//            }
+//            query += "(pubYear = \"" + props.getProperty("pubYear") + "\")";
+//        }
+//        if ((props.getProperty("bookTitle") == null) && (props.getProperty("author") == null) && (props.getProperty("pubYear") == null)){
+//            System.out.println("Error: no fields");
+//        }
+//
+//        query += " ORDER BY author";
+//
+////        System.out.println(query);
+//
+//        Vector allDataRetrieved = getSelectQueryResult(query);
+//
+//        if (allDataRetrieved != null)
+//        {
+//            bookList = new Vector<>();
+//
+//            for (int cnt = 0; cnt < allDataRetrieved.size(); cnt++)
+//            {
+//                Properties nextBookData = (Properties)allDataRetrieved.elementAt(cnt);
+//
+//                Book book = new Book(nextBookData);
+//
+//                bookList.add(book);
+//            }
+//
+//        }
+//        else
+//        {
+//            throw new Exception("No Books found with specified fields");
+//        }
+//
+//    }
+
+
+    // TODO: All these -v
+    @Override
+    public Object getState(String key) {
+        if (key.equals("Colors"))
+            return colorList;
+        else
+        if (key.equals("ColorCollection"))
+            return this;
+        return null;
+    }
+
+    @Override
+    public void stateChangeRequest(String key, Object value) {
+        myRegistry.updateSubscribers(key, this);
+    }
+
+    @Override
+    protected void initializeSchema(String tableName) {
+        if (mySchema == null)
+        {
+            mySchema = getSchemaInfo(tableName);
+        }
+    }
+
+    @Override
+    public void updateState(String key, Object value) {
+        stateChangeRequest(key, value);
+    }
 }
---
+++ userinterface/MessageView.java
@@ -0,0 +1,74 @@
+// tabs=4
+//************************************************************
+//	COPYRIGHT 2010/2015 Sandeep Mitra and Students, The
+//    College at Brockport, State University of New York. -
+//	  ALL RIGHTS RESERVED
+//
+// This file is the product of The College at Brockport and cannot
+// be reproduced, copied, or used in any shape or form without
+// the express written consent of The College at Brockport.
+//************************************************************
+//
+// specify the package
+package userinterface;
+
+// system imports
+
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+
+// project imports
+
+//==============================================================
+public class MessageView extends Text
+{
+
+	// Class constructor
+	//----------------------------------------------------------
+	public MessageView(String initialMessage)
+	{
+		super(initialMessage);
+		setFont(Font.font("Helvetica", FontWeight.BOLD, 16));
+		setFill(Color.BLUE);
+		setTextAlignment(TextAlignment.LEFT);
+	}
+
+	/**
+	 * Display ordinary message
+	 */
+	//----------------------------------------------------------
+	public void displayMessage(String message)
+	{
+		// display the passed text in red
+		setFill(Color.BLUE);
+		setText(message);
+	}
+
+	/**
+	 * Display error message
+	 */
+	//----------------------------------------------------------
+	public void displayErrorMessage(String message)
+	{
+		// display the passed text in red
+		setFill(Color.RED);
+		setText(message);
+	}
+
+	/**
+	 * Clear error message
+	 */
+	//----------------------------------------------------------
+	public void clearErrorMessage()
+	{
+		setText("                           ");
+	}
+
+
+}
+
+
+
