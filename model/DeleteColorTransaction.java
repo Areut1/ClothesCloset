d787293f88b25dc52a2bc282b8d7ce42fd92a6df
Adam <Areut1@brockport.edu>
2024-03-25 15:09:03 -0400

    Updated DeleteColorTransaction and ColorView

--- model/DeleteColorTransaction.java
+++ model/DeleteColorTransaction.java
@@ -107,7 +107,7 @@ public class DeleteColorTransaction extends Transaction{
         createAndShowView("SelectColorView");
 
     }
-    public void processConfirm(Properties props){
+    public void processConfirm(Properties props) {
 
         String id = props.getProperty("ColorId");
 
@@ -117,10 +117,7 @@ public class DeleteColorTransaction extends Transaction{
             throw new RuntimeException(e);
         }
         createAndShowView("DeleteColorView");
-
-
     }
-
 
 
 
