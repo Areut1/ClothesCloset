a1c49a3174552e0cb0ea4e0fbf823036bff5ccfa
ZackCharlebois <zcharlebois01@gmail.com>
2024-04-02 15:13:46 -0400

    Updated Vbox for css

--- userinterface/ModifyArticleTypeReceipt.java
+++ userinterface/ModifyArticleTypeReceipt.java
@@ -55,7 +55,7 @@ public class ModifyArticleTypeReceipt extends View
         // create a container for showing the contents
         VBox container = new VBox(10);
         container.setPadding(new Insets(15, 5, 5, 5));
-
+        container.getStyleClass().add("Vbox");
         // create our GUI components, add them to this panel
         container.getChildren().add(createTitle());
         container.getChildren().add(createFormContent());
